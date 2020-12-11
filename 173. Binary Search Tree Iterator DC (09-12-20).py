585330234cb95203756c011814e6b91a44e785f8
Dharaneeshwar <daranip@gmail.com>
2020-12-11 16:26:17 +0530

    One pass Solution for BST iterator

--- 173. Binary Search Tree Iterator DC (09-12-20).py
+++ 173. Binary Search Tree Iterator DC (09-12-20).py
@@ -1,3 +1,5 @@
+# Time Complexity - O(2n) ; 76 ms
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -28,6 +30,45 @@ class BSTIterator:
             return True 
         else:
             return False
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+# Time Complexity - O(n) ; 68 ms ; faster than 91.69 %
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        node = self.stack.pop() 
+        req_value = node.val 
+        # go right if possible 
+        if node.right:
+            node = node.right
+            # now moving left from the right node
+            while node:
+                self.stack.append(node)
+                node = node.left 
+        return req_value        
+
+    def hasNext(self) -> bool:
+        if self.stack:
+            return True 
+        return False
+
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
